d06712e23a48dc61e0b732d918bf8dfde461f9fb
parkjunyoung <joonyoung090@naver.com>
2023-04-26 20:53:20 +0900

    프로젝트 생성일

---
+++ src/main/java/site/jwhy/mrhometender/hometenter/HometenterApplication.java
@@ -0,0 +1,13 @@
+package site.jwhy.mrhometender.hometenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometenterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HometenterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/site/jwhy/mrhometender/hometenter/HometenterApplicationTests.java
@@ -0,0 +1,13 @@
+package site.jwhy.mrhometender.hometenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometenterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
